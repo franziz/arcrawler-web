f57d9c46d3a0112ad753800e356d7a22a3573aae
Frans Huang <franssiswanto@gmail.com>
2019-08-15 15:13:35 +0800

    introduction AppContainer for simple wrapper

--- src/pages/XpathTestPage/index.js
+++ src/pages/XpathTestPage/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 
-import Header from "components/Header";
-import DrawerMenu from "components/DrawerMenu";
-import Content from "components/Content";
+import AppContainer from "components/AppContainer";
 import ReactJson from "react-json-view";
 import { Grid, Typography, TextField, Button, Paper, CircularProgress } from "@material-ui/core";
 
@@ -35,54 +33,50 @@ export default function XpathTestPage(props){
   const classes = useStyles();
   
   return (
-    <div className={classes.root}>
-      <Header/>
-      <DrawerMenu/>
-      <Content>
-        <Grid direction="column" spacing={2} container>
-          <Grid xs={12} item>
-            <Typography variant="h6">XPATH Tools</Typography>
-            <Typography variant="caption">Use this tools to do XPATH testing. Under the hood, this tools will call ARCrawler API to perform XPATH extraction.</Typography>
-          </Grid>
-
-          <Grid xs={12} item>
-            <Grid direction="row" alignItems="center" spacing={2} container>
-              <Grid xs={2} lg={1} item>
-                <Typography>URL</Typography>
-              </Grid>
-              <Grid xs={10} lg={11} item>
-                <TextField value={url} onChange={handleChangeUrl} margin="dense" variant="outlined" fullWidth/>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid xs={12} item>
-            <Grid direction="row" alignItems="center" spacing={2} container>
-              <Grid xs={2} lg={1} item>
-                <Typography>XPATH</Typography>
-              </Grid>
-              <Grid xs={10} lg={11} item>
-                <TextField value={xpath} onChange={handleChangeXpath} margin="dense" variant="outlined" fullWidth/>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          <Grid xs={12} item>
-            <Button disabled={isExtracting} onClick={handleExtractClick}variant="contained" color="primary">
-              {isExtracting? <CircularProgress className={classes.circularProgress} size={16}/>: null} Extract
-            </Button>
-          </Grid>
-
-          <Grid xs={12} item>
-            <Grid direction="column" container>
-              <Typography variant="overline">Extraction Result</Typography>
-              <Paper className={classes.paper} elevation={4}>
-                <ReactJson name={false} src ={result}/>
-              </Paper>
-            </Grid>
-          </Grid>
-
+    <AppContainer>
+      <Grid direction="column" spacing={2} container>
+        <Grid xs={12} item>
+          <Typography variant="h6">XPATH Tools</Typography>
+          <Typography variant="caption">Use this tools to do XPATH testing. Under the hood, this tools will call ARCrawler API to perform XPATH extraction.</Typography>
         </Grid>
-      </Content>
-    </div>
+
+        <Grid xs={12} item>
+          <Grid direction="row" alignItems="center" spacing={2} container>
+            <Grid xs={2} lg={1} item>
+              <Typography>URL</Typography>
+            </Grid>
+            <Grid xs={10} lg={11} item>
+              <TextField value={url} onChange={handleChangeUrl} margin="dense" variant="outlined" fullWidth/>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid xs={12} item>
+          <Grid direction="row" alignItems="center" spacing={2} container>
+            <Grid xs={2} lg={1} item>
+              <Typography>XPATH</Typography>
+            </Grid>
+            <Grid xs={10} lg={11} item>
+              <TextField value={xpath} onChange={handleChangeXpath} margin="dense" variant="outlined" fullWidth/>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid xs={12} item>
+          <Button disabled={isExtracting} onClick={handleExtractClick}variant="contained" color="primary">
+            {isExtracting? <CircularProgress className={classes.circularProgress} size={16}/>: null} Extract
+          </Button>
+        </Grid>
+
+        <Grid xs={12} item>
+          <Grid direction="column" container>
+            <Typography variant="overline">Extraction Result</Typography>
+            <Paper className={classes.paper} elevation={4}>
+              <ReactJson name={false} src ={result}/>
+            </Paper>
+          </Grid>
+        </Grid>
+
+      </Grid>
+    </AppContainer>
   )
 }
